9c2b068dae307d141975ad4b2271aa259ced6c1d
Jake <49795711+Rarisma@users.noreply.github.com>
2022-02-20 20:42:35 +0000

    Fully impliment all tabs

--- StoryBuilder/Views/CharacterPage.xaml.cs
+++ StoryBuilder/Views/CharacterPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
+using StoryBuilder.Models;
 using StoryBuilder.ViewModels;
+using System;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,5 +18,16 @@ public sealed partial class CharacterPage : BindablePage
     {
         InitializeComponent();
         DataContext = CharVm;
+    }
+
+    private async void CloseTab(Microsoft.UI.Xaml.Controls.TabView sender, Microsoft.UI.Xaml.Controls.TabViewTabCloseRequestedEventArgs args)
+    {
+        ContentDialog CD = new();
+        CD.XamlRoot = GlobalData.XamlRoot;
+        CD.Content = new TextBlock() { Text = $"Are you sure you want to delete the {args.Tab.Header} tab" };
+        CD.Title = "Are you sure?";
+        CD.PrimaryButtonText = "Delete";
+        CD.SecondaryButtonText = "Cancel";
+        await CD.ShowAsync();
     }
 }
